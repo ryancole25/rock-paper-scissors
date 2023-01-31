e9ef13bf63d9318611c4d5d6ea4842906a2dff57
ryancole25 <cole.ryan.n@gmail.com>
2023-01-31 17:21:06 -0500

    Reconfigured JS, HTML, and CSS

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -1,91 +1,153 @@
 function getComputerChoice(){
     let choice = Math.floor(Math.random() * 3);
+    const computer = document.querySelector('.computer');
+    
     if (choice == 0){
-        return "Rock";
+        computer.textContent = "The Computer Chooses: Rock";
+        return "ROCK";
     }
     else if (choice == 1){
-        return "Paper";
+        computer.textContent = "The Computer Chooses: Paper";
+        return "PAPER";
     }
     else{
-        return "Scissors";
+        computer.textContent = "The Computer Chooses: Scissors";
+        return "SCISSORS";
     }
-}
-
-function getPlayerChoice(){
-    while (true){
-    let choice = prompt("rock, paper, or scissors?:");
-    if (choice.toUpperCase() != "ROCK" && choice.toUpperCase() != "PAPER" && choice.toUpperCase() != "SCISSORS"){
-        console.log("Invalid Response. Make sure you typed correctly.");
-        continue;
-    }
-    return choice;
-    }  
 }
 
 function playRound(playerSelection, computerSelection){
-    // console.log(`The computer picked ${computerSelection}.`);
-    if (playerSelection.toUpperCase() == "ROCK" && computerSelection.toUpperCase() == "ROCK"){
-        return "You tied. Rock ties Rock";
+    
+    if (playerSelection == "ROCK" && computerSelection == "ROCK"){
+        results.textContent = "You tied. Rock ties Rock"
+        return;
     }
-    else if (playerSelection.toUpperCase() == "ROCK" && computerSelection.toUpperCase() == "PAPER"){
-        return "You lose. Rock loses to Paper";
+    else if (playerSelection == "ROCK" && computerSelection == "PAPER"){
+        results.textContent = "You lose. Rock loses to Paper";
+        return;
     }
-    else if (playerSelection.toUpperCase() == "ROCK" && computerSelection.toUpperCase() == "SCISSORS"){
-        return "You win. Rock beats Scissors";
+    else if (playerSelection == "ROCK" && computerSelection == "SCISSORS"){
+        results.textContent = "You win. Rock beats Scissors"
+        return;
     }
-    else if (playerSelection.toUpperCase() == "PAPER" && computerSelection.toUpperCase() == "ROCK"){
-        return "You win. Paper beats Rock";
+    else if (playerSelection == "PAPER" && computerSelection == "ROCK"){
+        results.textContent = "You win. Paper beats Rock";
+        return;
     }
-    else if (playerSelection.toUpperCase() == "PAPER" && computerSelection.toUpperCase() == "PAPER"){
-        return "You tie. Paper ties Paper";
+    else if (playerSelection == "PAPER" && computerSelection == "PAPER"){
+        results.textContent = "You tie. Paper ties Paper"
+        return;
     }
-    else if (playerSelection.toUpperCase() == "PAPER" && computerSelection.toUpperCase() == "Scissors"){
-        return "You lose. Paper loses to Scissors";
+    else if (playerSelection == "PAPER" && computerSelection == "SCISSORS"){
+        results.textContent = "You lose. Paper loses to Scissors"
+        return;
     }
-    else if (playerSelection.toUpperCase() == "SCISSORS" && computerSelection.toUpperCase() == "ROCK"){
-        return "You lose. Scissors loses to Rock";
+    else if (playerSelection == "SCISSORS" && computerSelection == "ROCK"){
+        results.textContent = "You lose. Scissors loses to Rock";
+        return;
     }
-    else if (playerSelection.toUpperCase() == "SCISSORS" && computerSelection.toUpperCase() == "PAPER"){
-        return "You win. Scissors beats Rock";
+    else if (playerSelection == "SCISSORS" && computerSelection == "PAPER"){
+        results.textContent = "You win. Scissors beats Rock";
+        return;
     }
     else {
-        return "You lose. Scissors ties Scissors";
+        results.textContent = "You tie. Scissors ties Scissors";
+        return;
     }
 }
 
+function winner(wins){
+
+    if (results.textContent.slice(0,7) == "You win"){
+        wins += 1;
+
+        if (wins == 5){
+            win.textContent = `${wins}`;
+            const round = document.querySelector('.round');
+            round.textContent = "You won 5 games!";
+            return 0;
+        }
+        else if (wins < 5){
+            win.textContent = `${wins}`;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+function loser(losses){
+
+    if (results.textContent.slice(0,7) == "You los"){
+        losses += 1;
+
+        if (losses == 5){
+            loss.textContent = `${losses}`;
+            const round = document.querySelector('.round');
+            round.textContent = "You lost 5 games!";
+            return 0;
+        }
+        else if (losses < 5){
+            loss.textContent = `${losses}`;
+            return 1;
+        }
+    }
+    return 0;
+}
+
+function tied(ties){
+
+    if (results.textContent.slice(0,7) == "You tie"){
+        ties += 1;
+        tie.textContent = `${ties}`;
+        return 1;
+    }
+    return 0;
+}
+
+const results = document.querySelector(".results");
+const win = document.querySelector('.wins');
+const loss = document.querySelector('.losses');
+const tie = document.querySelector('.ties');
+
+
+let wins = 0;
+let losses = 0;
+let ties = 0;
+
+
 function game(){
-    let wins = 0;
-    let losses = 0;
-    let ties = 0;
+    
+    const rock = document.querySelector(".rock");
+    rock.addEventListener('click',function() {
+        playRound("ROCK", getComputerChoice())
+        wins += winner(wins);
+        losses += loser(losses);
+        ties += tied(ties);
+        });
+    
+    const paper = document.querySelector(".paper");
+    paper.addEventListener("click", function() {
+        playRound("PAPER", getComputerChoice());
+        wins += winner(wins);
+        losses += loser(losses);
+        ties += tied(ties);
+        });
+    
+    const scissors = document.querySelector(".scissors");
+        scissors.addEventListener("click", function() {
+        playRound("SCISSORS", getComputerChoice());
+        wins += winner(wins);
+        losses += loser(losses);
+        ties += tied(ties);
+        });
 
-    for (let i = 0; i < 5; i ++){
-        let playerSelection = getPlayerChoice();
-        let computerSelection = getComputerChoice();
-        console.log(`The computer picked ${computerSelection}`);
-        if (playRound(playerSelection, computerSelection).slice(0,7) == "You win"){
-            wins += 1;
-            console.log(playRound(playerSelection, computerSelection));
-        }
-        else if (playRound(playerSelection, computerSelection).slice(0,7) == "You los"){
-            losses += 1;
-            console.log(playRound(playerSelection, computerSelection));
-        }
-        else{
-            ties += 1;
-            console.log(playRound(playerSelection, computerSelection));
-        }
-        console.log(`SCOREBOARD: ${wins} win(s), ${losses} loss(es), ${ties} tie(s)`);
-    }
 
-    if (wins > losses){
-        console.log("You won the series!");
-    }
-    else if (wins < losses){
-        console.log("You lost the series.");
-    }
-    else {
-        console.log("You tied the series.");
-    }
 }
 
 game();
+
+const restart = document.querySelector('.restart');
+restart.addEventListener('click', function() {
+    wins, losses, ties = 0, 0, 0;
+    location.reload();
+});
